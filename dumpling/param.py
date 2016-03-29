958cc098d34f089d072333efe187c3cc2dcf8108
Zech Xu <zhenjiang.xu@gmail.com>
2016-03-28 21:35:32 -0700

    delete attr setter and getter in OptionParam
    add util functions and Parameters and Dumpling classes

--- dumpling/param.py
+++ dumpling/param.py
@@ -1,13 +1,38 @@
 from keyword import iskeyword
+from collections import OrderedDict
 
 
-class ArgParam:
+def check_choice(it):
+    def func(v):
+        if v not in it:
+            msg = 'Illegal value: {}'
+            raise ValueError(msg.format(v))
+        return v
+    return func
+
+
+def check_range(minimum, maximum):
+    def func(v):
+        if not minimum < v < maximum:
+            msg = 'Illegal value: {}'
+            raise ValueError(msg.format(v))
+        return v
+    return func
+
+
+class ArgmntParam:
     '''Class of arguement parameter.
     '''
 
 
 class OptionParam:
     '''Class of option parameter.
+
+    Parameters
+    ----------
+    formatter : callable
+        To validate and tokenize the `value` into right format.
+        For example, if `value` is a file path, use `shlex.quote`.
     '''
     def __init__(self, name, alias=None, value=None, formatter=lambda i: i,
                  help='', delimiter=' '):
@@ -54,36 +79,66 @@ class OptionParam:
         s = '<{}{}{}   "{}">'
         return s.format(self.name, self.delimiter, self.value, self.help)
 
+    def __str__(self):
+        if self.value is False or self.value is None:
+            return ''
+        elif self.value is True:
+            return self.name
+        else:
+            return '{}{}{}'.format(self.name, self.delimiter, self.value)
+
     def __eq__(self, other):
         ''''''
-        return other.name == self.name and other.value == other.value
-
-    def __setattr__(self, n, v):
-        '''Override to keep the class attributes immutable.'''
-        if n == 'value' or not hasattr(self, n):
-            super().__setattr__(n, v)
-        else:
-            msg = 'Attribute {} of the {} class cannot be changed'
-            raise AttributeError(msg.format(n, type(self).__name__))
-
-    def __delattr__(self, *args):
-        '''Override to prevent the class attributes from being deleted.'''
-        msg = 'Attributes {} of the {} class cannot be deleted'
-        raise AttributeError(msg.format(n, type(self).__name__))
+        return other.name == self.name and other.value == self.value
 
     def is_on(self):
-        return self.value is None
+        return self.value is not None
 
     def on(self, value):
         self.value = value
 
 
-class Parameters:
-    def __init__(self, params):
-        self.params = params
+class Parameters(OrderedDict):
+    @classmethod
+    def from_params(cls, params):
+        return cls(yield from [(param.name, param), (param.alias, param)]
+                   for param in params)
 
-    def __getitem__(self, key):
+    def __setitem__(self, name, value):
+        self[name].on(value)
+        self[self.alias_map[name]].on(value)
+
+    def __getitem__(self, name):
+        if name in self:
+            return self[name]
+        elif self[self.alias_map[name]] in self:
+            return self[self.alias_map[name]]
+
+    def __repr__(self):
+        ''''''
+
+
+class Dumpling:
+    def __init__(self, cmd, params,
+                 cwd, tmp_dir,
+                 stdin, stdout, stderr,
+                 cmd_delimiter=' ',
+                 version='', url=''):
+        self.version = version
+
+    @property
+    def command(self):
         return
 
-    def __setitem__(self, key, value):
-        self
+    def __call__(**kwargs):
+        ''''''
+
+    def __str__(self):
+        ''''''
+        items = []
+        for k in self.params:
+            p = self.params[k]
+            s = str(s)
+            if s:
+                items.append(s)
+        return self.delimiter.join(items)
