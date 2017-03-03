5da54fb14b9c0cc4623bbd794c35eecd2ad27fdd
Zech Xu <zhenjiang.xu@gmail.com>
2017-03-03 15:38:39 -0800

    finalize the doc publish

--- publish.sh
+++ publish.sh
@@ -2,13 +2,16 @@
 
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
+# clear the old html if there is any
 make -C doc clean
+# clone the gh-pages branch so we can keep its commit history
 git clone -b gh-pages --single-branch https://$GH_TOKEN@github.com/RNAer/dumpling.git doc/_build/html
+# update the html doc
 make -C doc html
+# go to the html dir to commit changes and upload
 cd doc/_build/html
-pwd
-ls
-git remote -v
 git status
-git commit -a -m "$1"
+# add and commit all changes
+git add *
+git commit -m "$1"
 git push --quiet origin gh-pages
