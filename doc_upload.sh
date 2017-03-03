e00b3f35edc4006ffefc4c9f5caff9f21d7e0870
Zech Xu <zhenjiang.xu@gmail.com>
2017-03-03 14:37:37 -0800

    you have to set up git user config

--- doc_upload.sh
+++ doc_upload.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+git config --global user.email "zhenjiang.xu@gmail.com"
+git config --global user.name "Zech Xu"
 make -C doc clean
 git clone -b gh-pages --single-branch https://$GH_TOKEN@github.com/biocore/calour.git doc/_build/html
 make -C doc html
 cd doc/_build/html
+
 git commit -a -m "$1"
 git push --quiet origin gh-pages
