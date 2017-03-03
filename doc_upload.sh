d9373b1a0f8a5684950b3d155e6565f1fac0dd30
Zech Xu <zhenjiang.xu@gmail.com>
2017-03-03 15:01:31 -0800

    replace with git env var

--- doc_upload.sh
+++ doc_upload.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-git config --global user.email "zhenjiang.xu@gmail.com"
-git config --global user.name "Zech Xu"
+git config --global user.email "$GIT_EMAIL"
+git config --global user.name "$GIT_NAME"
 make -C doc clean
-git clone -b gh-pages --single-branch https://$GH_TOKEN@github.com/biocore/calour.git doc/_build/html
+git clone -b gh-pages --single-branch https://$GH_TOKEN@github.com/RNAer/dumpling.git doc/_build/html
 make -C doc html
 cd doc/_build/html
-
+pwd
+ls
+git remote -v
+git status
 git commit -a -m "$1"
 git push --quiet origin gh-pages
